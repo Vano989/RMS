95dbba5dbc44fadb50c64e904fb3e57f57153387
ivash <ivashka7@inbox.lv>
2026-01-08 12:42:51 +0200

    Remove temporary files

--- SQLQuery1.sql
+++
@@ -1,9 +0,0 @@
-create table users
-(
-	ID int primary key identity,
-	username varchar(50) not null unique, 
-	pass varchar(10) not null,
-	fullName varchar(50) not null,
-	phoneNumber varchar(15),
-)
-
